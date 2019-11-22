55689def1798145545f5c4e1686e435a6090fcc2
Sanpi <sanpi@homecomputing.fr>
2019-11-22 14:08:37 +0100

    i3 implementation

---
+++ src/i3.rs
@@ -0,0 +1,74 @@
+pub(crate) struct Wm {
+    client: i3_ipc::I3Stream,
+}
+
+impl Wm {
+    fn active_workspace(&mut self) -> Option<usize>
+    {
+        let workspaces = match self.client.get_workspaces() {
+            Ok(workspaces) => workspaces,
+            Err(_) => return None,
+        };
+
+        for workspace in workspaces {
+            if workspace.focused {
+                return Some(workspace.num);
+            }
+        }
+
+        None
+    }
+}
+
+impl crate::Wm for Wm {
+    type Output = i3_ipc::reply::Output;
+
+    fn new() -> crate::Result<Self>
+    {
+        use i3_ipc::Connect;
+
+        let client = i3_ipc::I3::connect()?;
+
+        Ok(Self {
+            client
+        })
+    }
+
+    fn outputs(&mut self) -> crate::Result<Vec<Self::Output>>
+    {
+        let outputs = self.client.get_outputs()?;
+
+        Ok(outputs)
+    }
+
+    fn active_output(&mut self, outputs: &[Self::Output]) -> Option<usize>
+    {
+        let active_workspace = match self.active_workspace() {
+            Some(active_workspace) => active_workspace,
+            None => return None,
+        };
+
+        for (n, output) in outputs.iter().enumerate() {
+            if output.current_workspace == Some(active_workspace.to_string()) {
+                return Some(n);
+            }
+        }
+
+        None
+    }
+
+    fn is_active(&mut self, output: &Self::Output) -> bool
+    {
+        output.active
+    }
+
+    fn select_current_workspace(&mut self, output: &Self::Output) -> crate::Result<()>
+    {
+        let workspace = output.current_workspace.as_ref().unwrap();
+        let command = format!("workspace {}", workspace);
+
+        self.client.run_command(&command)?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+mod i3;
+
+trait Wm {
+    type Output;
+
+    fn new() -> crate::Result<Self> where Self: Sized;
+    fn outputs(&mut self) -> crate::Result<Vec<Self::Output>>;
+    fn active_output(&mut self, outputs: &[Self::Output]) -> Option<usize>;
+    fn is_active(&mut self, output: &Self::Output) -> bool;
+    fn select_current_workspace(&mut self, output: &Self::Output) -> crate::Result<()>;
+}
+
+#[derive(derive_more::From, Debug)]
+enum Error {
+    NotFound,
+    Io(std::io::Error),
+    Utf8(std::str::Utf8Error),
+    Json(json::Error),
+}
+
+type Result<T> = std::result::Result<T, Error>;
+
+fn main() -> Result<()>
+{
+    let mut client = i3::Wm::new()?;
+    let outputs = client.outputs()?;
+
+    if let Some(mut active) = client.active_output(&outputs) {
+        loop {
+            active = (active + 1) % outputs.len();
+            if client.is_active(&outputs[active]) == true {
+                break;
+            }
+        }
+
+        client.select_current_workspace(&outputs[active])?;
+    }
+
+    Ok(())
 }
